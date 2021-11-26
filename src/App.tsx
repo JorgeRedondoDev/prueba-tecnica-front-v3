f2ccecac539ad76ed95fe4d246c5692a90cba122
Jorge <jorgeRedondoDev@gmail.com>
2021-11-26 14:09:58 +0100

    Protected routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
 import React from "react";
+import Login from "pages/Login";
+import Listado from "pages/Listado";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 const App = () => {
+  const isLogged: Boolean =
+    localStorage?.getItem("token") === "QpwL5tke4Pnpja7X4";
+
   return (
-    <div>
-      <p>Prueba técnica de LaLiga</p>
-    </div>
+    <Router>
+      <Switch>
+        <Route
+          path="/users"
+          component={
+            isLogged ? () => <Listado /> : () => <Redirect to="/login" />
+          }
+        />
+        <Route
+          path="/login"
+          component={
+            isLogged ? () => <Redirect to="/users" /> : () => <Login />
+          }
+        />
+        {isLogged ? <Redirect to="/users" /> : <Redirect to="/login" />}
+      </Switch>
+    </Router>
   );
 };
 
---
+++ src/pages/Listado.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Listado() {
+  return <div>List</div>;
+}
+
+export default Listado;
