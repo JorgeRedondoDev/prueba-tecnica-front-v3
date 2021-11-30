110e7fbc1081e99096db024995ddc9f7cc188117
Jorge <jorgeRedondoDev@gmail.com>
2021-11-30 11:16:47 +0100

    fixed styles

--- src/components/UserModal.tsx
+++ src/components/UserModal.tsx
@@ -14,47 +14,66 @@ export default function UserModal() {
     return () => document.removeEventListener("click", handleClick);
 
     function handleClick(e) {
-      console.log(e.target.id);
       if (e.target.id !== "modal") dispatch(requestModal({}));
     }
   });
 
   return (
-    <Padre>
+    <Container>
       <ModalCard id="modal">
-        <button onClick={() => dispatch(requestModal({}))}>Click</button>
-        <img src={modalData.avatar} alt="modal" />
-        <p> {modalData.email} </p>
-        <p>
+        <button onClick={() => dispatch(requestModal({}))}>
+          <span role="img" aria-label="close">
+            ❌
+          </span>
+        </button>
+        <img src={modalData.avatar} alt="modal" id="modal" />
+        <p id="modal"> {modalData.email} </p>
+        <p id="modal">
           {modalData.first_name} {modalData.last_name}
         </p>
       </ModalCard>
-    </Padre>
+    </Container>
   );
 }
 
-const Padre = styled.div`
+const Container = styled.div`
   position: fixed;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: rgba(0, 0, 0, 0.2);
+  background-color: rgba(0, 0, 0, 0.5);
 `;
 const ModalCard = styled.div`
   position: fixed;
   border-radius: 5px;
   padding: 20px;
-  background-color: rgba(0, 0, 0, 0.2);
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  width: 300px;
-  height: 200px;
+  width: 200px;
+  height: 230px;
 
-  .modal-content {
+  border: 1px solid #fff;
+  background-color: rgba(157, 170, 242, 0.7);
+
+  img {
+    border-radius: 5px;
+    margin-bottom: 30px;
+  }
+  button {
+    background-color: transparent;
+    border: none;
+    font-size: 20px;
+    &:hover {
+      cursor: pointer;
+    }
   }
 
+  p {
+    margin: 10px 0;
+    text-align: center;
+  }
   button {
     float: right;
   }
